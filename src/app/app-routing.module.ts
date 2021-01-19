36508c2a24d036c16a8ed45e2b32645bc656b474
philip loeffler <philip.loeffler@ascendum.com>
2021-01-19 07:32:32 -0500

    restart fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserLoginComponent } from './Components/user/user-login/user-login.component';
+import { UserListComponent } from './Components/user/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-login', component: UserLoginComponent },
+  { path: 'user-list', component: UserListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './Components/user/user-list/user-list.component';
+import { UserLoginComponent } from './Components/user/user-login/user-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
