981ec4d32a92db0c35580162c26fa5a84ed6f76a
philip loeffler <philip.loeffler@ascendum.com>
2021-01-19 16:08:25 -0500

    fixed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './Components/user/user-list/user-list.component';
 import { UserLoginComponent } from './Components/user/user-login/user-login.component';
 
 @NgModule({
   declarations: [AppComponent, UserListComponent, UserLoginComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
