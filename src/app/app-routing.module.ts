8996edc1a9561e20040bbc9df968914d491a1f79
philip loeffler <philip.loeffler@ascendum.com>
2021-01-30 08:18:13 -0500

    fixed folder struc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,14 +13,13 @@ import { VendorListComponent } from './Components/vendor/vendor-list/vendor-list
 import { VendorDetailComponent } from './Components/vendor/vendor-detail/vendor-detail.component';
 import { VendorEditComponent } from './Components/vendor/vendor-edit/vendor-edit.component';
 import { VendorCreateComponent } from './Components/vendor/vendor-create/vendor-create.component';
-
 import { PurchaseRequestListComponent } from './Components/purchase-request/purchase-request-list/purchase-request-list.component';
 import { PurchaseRequestDetailComponent } from './Components/purchase-request/purchase-request-detail/purchase-request-detail.component';
 import { PurchaseRequestEditComponent } from './Components/purchase-request/purchase-request-edit/purchase-request-edit.component';
 import { PurchaseRequestCreateComponent } from './Components/purchase-request/purchase-request-create/purchase-request-create.component';
-
 import { PurchaseRequestLinesComponent } from './Components/purchase-request/purchase-request-lines/purchase-request-lines.component';
-
+import { LineItemEditComponent } from './Components/line-item/line-item-edit/line-item-edit.component';
+import { LineItemCreateComponent } from './Components/line-item/line-item-create/line-item-create.component';
 const routes: Routes = [
   { path: 'user-login', component: UserLoginComponent },
   { path: 'user-list', component: UserListComponent },
@@ -35,6 +34,8 @@ const routes: Routes = [
   { path: 'vendor-detail/:id', component: VendorDetailComponent },
   { path: 'vendor-edit/:id', component: VendorEditComponent },
   { path: 'vendor-create', component: VendorCreateComponent },
+  { path: 'line-item-edit/:id', component: LineItemEditComponent },
+  { path: 'line-item-create', component: LineItemCreateComponent },
   { path: 'purchase-request-list', component: PurchaseRequestListComponent },
   {
     path: 'purchase-request-detail/:id',
